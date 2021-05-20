b3fd5413205f759e17cd49bc0a8ab18268aff306
Chris <christophertberg@gmail.com>
2021-05-19 22:25:49 -0700

    completed #66 Plus One

---
+++ plus_one.py
@@ -0,0 +1,20 @@
+class Solution:
+    def plusOne(self, digits):
+        """
+        Increment the number derived from joining the integers of a list and return a list of the digits.
+        :param digits: List of digits
+        :return: List of digits
+        """
+
+        digits = [str(x) for x in digits]
+        digit = int(''.join(digits))
+        digit += 1
+
+        incremented_digits = [int(x) for x in str(digit)]
+
+        return incremented_digits
+
+test = [9, 9, 9]
+answer = Solution().plusOne(test)
+
+print(answer)
